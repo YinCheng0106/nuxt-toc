fd82b1060f791f1ffa3a661ad7c68dcb7e74b40f
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-08-21 16:56:12 +0800

    fix: make the module usable

--- src/module.ts
+++ src/module.ts
@@ -1,12 +1,15 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver, addComponent } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: 'nuxt-toc',
+    configKey: 'nuxt-toc',
+    compatibility: {
+      nuxt: '>= 3.0.0'
+    }
   },
   // Default configuration options of the Nuxt module
   defaults: {},
@@ -14,6 +17,11 @@ export default defineNuxtModule<ModuleOptions>({
     const resolver = createResolver(import.meta.url)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    // addPlugin(resolver.resolve('./runtime/plugin'))
+
+    addComponent({
+      name: 'TableOfContents',
+      filePath: resolver.resolve('runtime/components/TableOfContents.vue')
+    })
   },
 })
