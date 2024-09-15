85fd4b91d8deaebb1330907d8cd5b813c4ce6725
hanyujie2002 <84226578+hanyujie2002@users.noreply.github.com>
2024-09-15 19:19:00 +0800

    refact: Use CustomQuery instead to improve performance

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { defineNuxtModule, addPlugin, createResolver, addComponent } from '@nuxt/kit'
+import { defineNuxtModule, createResolver, addComponent } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
@@ -8,20 +8,17 @@ export default defineNuxtModule<ModuleOptions>({
     name: 'nuxt-toc',
     configKey: 'nuxt-toc',
     compatibility: {
-      nuxt: '>= 3.0.0'
-    }
+      nuxt: '>= 3.0.0',
+    },
   },
   // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
     const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    // addPlugin(resolver.resolve('./runtime/plugin'))
-
     addComponent({
       name: 'TableOfContents',
-      filePath: resolver.resolve('runtime/components/TableOfContents.vue')
+      filePath: resolver.resolve('runtime/components/TableOfContents.vue'),
     })
   },
 })
--- src/runtime/components/TableOfContents.vue
+++ src/runtime/components/TableOfContents.vue
@@ -1,73 +1,66 @@
 <template>
-  <div class="flex mb-auto">
-    <ul v-if="toc && toc.links" class="flex flex-col">
-      <li v-for="link in toc.links" :key="link.text">
-        <a :href="`#${link.id}`" :class="{ active: activeTocIds.has(link.id) || link.id === lastVisibleHeading }">{{ link.text }}</a>
+  <CustomQuery class="flex mb-auto" :path="$route.path" :only="['body']" find="one" v-slot="{ data }">
+    <ul v-if="data.body.toc && data.body.toc.links" class="flex flex-col">
+      <li v-for="link in data.body.toc.links" :key="link.text">
+        <a :href="`#${link.id}`" :class="{ active: activeTocIds.has(link.id) || link.id === lastVisibleHeading }">{{
+          link.text }}</a>
         <ul v-if="link.children && link.children.length">
           <li v-for="child in link.children" :key="child.id" class="ml-3">
-            <a :href="`#${child.id}`" :class="{ active: activeTocIds.has(child.id) || child.id === lastVisibleHeading }"> {{child.text}}</a>
+            <a :href="`#${child.id}`"
+              :class="{ active: activeTocIds.has(child.id) || child.id === lastVisibleHeading }"> {{ child.text }}</a>
           </li>
         </ul>
       </li>
     </ul>
-  </div>
+  </CustomQuery>
 </template>
 
 <script setup lang="ts">
-import { useRoute, queryContent, ref, onMounted, onUnmounted } from '#imports'
+import { ref, onMounted, onUnmounted } from '#imports'
+import CustomQuery from './CustomQuery.vue';
 
-const route = useRoute()
-const path: string = route.path;
-const doc = await queryContent(path).findOne()
-const toc = ref();
-
-if (doc.body === null) {
-  toc.value = null
-} else {
-  toc.value = doc.body.toc
-}
-
-const activeTocIds = ref<Set<string>>(new Set());
-const lastVisibleHeading = ref<string>('');
+const activeTocIds = ref<Set<string>>(new Set())
+const lastVisibleHeading = ref<string>('')
 
 const observeSections = () => {
   const options = {
     root: null,
     rootMargin: '0px 0px -80% 0px',
-    threshold: [0, 0.25, 0.5, 0.75, 1]
-  };
+    threshold: [0, 0.25, 0.5, 0.75, 1],
+  }
 
   const callback: IntersectionObserverCallback = async (entries: IntersectionObserverEntry[]): Promise<void> => {
     entries.forEach((entry) => {
-      const id: string = entry.target.id;
+      const id: string = entry.target.id
       if (entry.isIntersecting) {
-        lastVisibleHeading.value = id;
-        activeTocIds.value.add(id);
-      } else {
-        activeTocIds.value.delete(id);
+        lastVisibleHeading.value = id
+        activeTocIds.value.add(id)
       }
-    });
-  };
+      else {
+        activeTocIds.value.delete(id)
+      }
+    })
+  }
 
-  const observer: IntersectionObserver = new IntersectionObserver(callback, options);
-  const sections: NodeListOf<HTMLElement> = document.querySelectorAll('h2, h3');
+  const observer: IntersectionObserver = new IntersectionObserver(callback, options)
+  const sections: NodeListOf<HTMLElement> = document.querySelectorAll('h2, h3')
 
   sections.forEach((section: HTMLElement) =>
-    observer.observe(section));
+    observer.observe(section))
 
   onUnmounted((): void => {
     sections.forEach((section: HTMLElement) =>
-      observer.unobserve(section));
+      observer.unobserve(section))
   })
 }
 
 onMounted((): void => {
-  observeSections();
+  observeSections()
 })
 </script>
 
 <style scoped>
 .active {
-  @apply text-yellow-200;
+  @apply text-yellow-200
 }
 </style>
